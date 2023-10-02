31108e3641f86be73d9998efcc8474feeb68d6d6
pandakn <natthawut.klangyod@gmail.com>
2023-10-02 18:19:12 +0700

    -feature🎉: add menu controller

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,14 @@
 import express, { Router } from "express";
 import helloRoute from "./hello.route";
 import usersRoute from "./users.route";
+import menusRoute from "./menus.route";
 
 const router = Router();
 
 export default (): express.Router => {
   helloRoute(router);
   usersRoute(router);
+  menusRoute(router);
 
   return router;
 };
