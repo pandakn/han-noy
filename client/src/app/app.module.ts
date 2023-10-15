da828e4412702abce6edf70ca4a80db303400fef
pandakn <natthawut.klangyod@gmail.com>
2023-10-15 16:44:09 +0700

    -fix⛏: conflict

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,14 +18,10 @@ import { MenuCardComponent } from "./components/room/menu-card/menu-card.compone
 import { AddMenuComponent } from "./components/bill/add-menu/add-menu.component";
 import { UpdateRoomComponent } from "./components/room/update-room/update-room.component";
 import { FilterPipe } from "./filter/filter.pipe";
-<<<<<<< HEAD
 import { SearchBarComponent } from "./components/search-bar/search-bar.component";
-import { UpdatePayerComponent } from './components/bill/update-payer/update-payer.component';
-=======
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { FriendComponent } from "./components/room/friend/friend.component";
-import { ShowQrcodeComponent } from './components/room/show-qrcode/show-qrcode.component';
->>>>>>> 6001356e6a7c117b9e2387900cb114cc1ffdd922
+import { ShowQrcodeComponent } from "./components/room/show-qrcode/show-qrcode.component";
+import { UpdatePayerComponent } from "./components/bill/update-payer/update-payer.component";
 
 @NgModule({
     declarations: [
@@ -41,12 +37,9 @@ import { ShowQrcodeComponent } from './components/room/show-qrcode/show-qrcode.c
         UpdateRoomComponent,
         FilterPipe,
         SearchBarComponent,
-<<<<<<< HEAD
-        UpdatePayerComponent,
-=======
         FriendComponent,
-        ShowQrcodeComponent
->>>>>>> 6001356e6a7c117b9e2387900cb114cc1ffdd922
+        ShowQrcodeComponent,
+        UpdatePayerComponent,
     ],
     imports: [
         CommonModule,
